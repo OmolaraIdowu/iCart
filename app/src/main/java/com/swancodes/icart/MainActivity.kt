df9f32c01724ce3f2fb2d984c86fc91faba735d2
Omolara Idowu <omolaraidowu44@gmail.com>
2023-03-13 13:16:20 +0100

    Initial commit

---
+++ app/src/main/java/com/swancodes/icart/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.swancodes.icart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
