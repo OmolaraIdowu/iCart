4c2c79028abc039e109428723b42143c190b024b
Omolara Idowu <omolaraidowu44@gmail.com>
2023-03-18 11:09:27 +0100

    Fixed bottom navigation view

--- app/src/main/java/com/swancodes/icart/ui/cart/CartFragment.kt
+++ app/src/main/java/com/swancodes/icart/ui/cart/CartFragment.kt
@@ -1,4 +1,4 @@
-package com.swancodes.icart.ui.profile
+package com.swancodes.icart.ui.cart
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,13 +7,13 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.swancodes.icart.R
 
-class ProfileFragment : Fragment() {
+class CartFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 }
